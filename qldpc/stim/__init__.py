00d63c093715344555423fa8735d392ad17def27
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-07-31 19:01:15 -0400

    add qldpc.stim.noise_model

---
+++ qldpc/stim/__init__.py
@@ -0,0 +1,8 @@
+from .noise_model import DepolarizingNoiseModel, NoiseModel, NoiseRule, SI1000NoiseModel
+
+__all__ = [
+    "DepolarizingNoiseModel",
+    "NoiseModel",
+    "NoiseRule",
+    "SI1000NoiseModel",
+]
