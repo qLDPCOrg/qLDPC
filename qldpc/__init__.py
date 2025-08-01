5cfd440797d57b07bbdfb32a5fbd47bdabb63dbf
Michael A. Perlin <michael.perlin@jpmchase.com>
2025-07-31 20:53:59 -0400

    move circuits.py to stim/circuits.py

--- qldpc/__init__.py
+++ qldpc/__init__.py
@@ -1,7 +1,8 @@
 import importlib.metadata
 
-from . import abstract, cache, circuits, codes, decoders, external, math, objects, stim
+from . import abstract, cache, codes, decoders, external, math, objects, stim
 
+circuits = stim  # alias
 __version__ = importlib.metadata.version("qldpc")
 
 __all__ = [
--- qldpc/stim/__init__.py
+++ qldpc/stim/__init__.py
@@ -1,6 +1,29 @@
-from .noise_model import DepolarizingNoiseModel, NoiseModel, NoiseRule, SI1000NoiseModel
+from .circuits import (
+    get_encoder_and_decoder,
+    get_encoding_circuit,
+    get_encoding_tableau,
+    get_logical_tableau,
+    get_transversal_automorphism_group,
+    get_transversal_circuit,
+    get_transversal_circuits,
+    get_transversal_ops,
+)
+from .noise_model import (
+    DepolarizingNoiseModel,
+    NoiseModel,
+    NoiseRule,
+    SI1000NoiseModel,
+)
 
 __all__ = [
+    "get_encoder_and_decoder",
+    "get_encoding_circuit",
+    "get_encoding_tableau",
+    "get_logical_tableau",
+    "get_transversal_automorphism_group",
+    "get_transversal_circuit",
+    "get_transversal_circuits",
+    "get_transversal_ops",
     "DepolarizingNoiseModel",
     "NoiseModel",
     "NoiseRule",
