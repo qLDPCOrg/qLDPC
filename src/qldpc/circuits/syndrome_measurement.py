d7b9d8336dd2781a5c90a5081aedd29e8e5e0fc7
Abbas Bracken Ziad <abbas.ziad25@imperial.ac.uk>
2026-03-10 00:14:26 +0000

    Prevent Stim from merging scheduled gate layers

--- src/qldpc/circuits/syndrome_measurement.py
+++ src/qldpc/circuits/syndrome_measurement.py
@@ -134,6 +134,8 @@ class EdgeColoring(SyndromeMeasurementStrategy):
         for gates in color_to_ops.values():
             for gate, check_id, data_id in sorted(gates):
                 circuit.append(gate, [check_id, data_id])
+            # Use TICK to prevent Stim from merging scheduled gate layers. 
+            circuit.append("TICK")
         return circuit
 
 
