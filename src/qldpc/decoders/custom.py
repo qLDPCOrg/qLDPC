c0c6ded0a900c9e77de6089b3d466c1f5c5b6aad
Michael A. Perlin <michael.perlin@jpmchase.com>
2026-03-28 12:13:23 -0400

    provide helpful error message for breaking change

--- src/qldpc/decoders/custom.py
+++ src/qldpc/decoders/custom.py
@@ -109,6 +109,12 @@ class RelayBPDecoder(BatchDecoder):
                 f"Relay-BP decoder name not recognized: {name}\n"
                 "See 'import relay_bp; help(relay_bp.bp)' for available Relay-BP decoders"
             )
+        if isinstance(pcm_or_dem, str):  # pragma: no cover
+            raise ValueError(
+                "I think you provided a Relay-BP decoder decoder name in place of a parity check"
+                " matrix. There was breaking change to this API."
+                " See help(qldpc.decoders.RelayBPDecoder)"
+            )
 
         # extract relevant data from a detector error model
         if isinstance(pcm_or_dem, stim.DetectorErrorModel):
