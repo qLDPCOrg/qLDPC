042c548c502c8380c554008d9f185ab9d060ba08
Michael A. Perlin <michael.perlin@jpmchase.com>
2026-04-08 20:57:03 -0400

    future annotations

--- src/qldpc/circuits/benchmarking.py
+++ src/qldpc/circuits/benchmarking.py
@@ -15,6 +15,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+from __future__ import annotations
+
 from collections.abc import Callable, Sequence
 
 import numpy as np
