qLDPCOrg/qLDPC

commit 026db8899eeec767e33c376785c24afb0609b284

Recycle X and Z decoders if possible in `CSSCode.get_logical_error_rate_func` (#475)
* recycle x and z decoders when computing logical error rates of self-dual CSS codes

* ignore coverage

src/qldpc/codes/common.py
@@ -3090,12 +3090,20 @@ class CSSCode(QuditCode):
 
         stabilizer_ops_x = self.get_stabilizer_ops(Pauli.X, canonicalized=False)
         stabilizer_ops_z = self.get_stabilizer_ops(Pauli.Z, canonicalized=False)
+        same_x_and_z = (
+            np.array_equal(stabilizer_ops_x, stabilizer_ops_z),
+            decoder_x_kwargs == decoder_z_kwargs,
+        )
 
         # construct decoders
         decoder_x_kwargs = (decoder_x_kwargs or {}) | decoder_kwargs
         decoder_z_kwargs = (decoder_z_kwargs or {}) | decoder_kwargs
-        decoder_x = decoders.get_decoder(stabilizer_ops_z, **decoder_kwargs)
-        decoder_z = decoders.get_decoder(stabilizer_ops_x, **decoder_kwargs)
+        decoder_x = decoders.get_decoder(stabilizer_ops_z, **decoder_x_kwargs)
+        decoder_z = (
+            decoder_x
+            if same_x_and_z
+            else decoders.get_decoder(stabilizer_ops_x, **decoder_z_kwargs)
+        )
 
         # identify logical operators
         logicals_x = self.get_logical_ops(Pauli.X)

src/qldpc/abstract/rings.py
@@ -1347,7 +1347,7 @@ class WedderburnArtinComponentTransformer:
         """
         idempotent = RingMember.from_vector(idempotent_vec, self.ring)
         subalgebra_basis = idempotent.adjoint_lift().column_space()
-        if len(subalgebra_basis) == self.degree:
+        if len(subalgebra_basis) == self.degree:  # pragma: no cover (we may not hit this in tests)
             return idempotent_vec.reshape(1, -1).view(self.field)
 
         """

commit 080d38378817465d1d4aff22a51fbac66b71f7bc

fix bug introduced in #475

src/qldpc/codes/common.py
@@ -3091,8 +3091,8 @@ class CSSCode(QuditCode):
         stabilizer_ops_x = self.get_stabilizer_ops(Pauli.X, canonicalized=False)
         stabilizer_ops_z = self.get_stabilizer_ops(Pauli.Z, canonicalized=False)
         same_x_and_z = (
-            np.array_equal(stabilizer_ops_x, stabilizer_ops_z),
-            decoder_x_kwargs == decoder_z_kwargs,
+            np.array_equal(stabilizer_ops_x, stabilizer_ops_z)
+            and decoder_x_kwargs == decoder_z_kwargs
         )
 
         # construct decoders