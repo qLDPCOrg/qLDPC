36d029c9f772fe17995762f95996f2698234a96f
Michael A. Perlin <michael.perlin@jpmchase.com>
2026-05-18 08:24:01 -0400

    docstrings

--- src/qldpc/abstract/conftest.py
+++ src/qldpc/abstract/conftest.py
@@ -13,4 +13,5 @@ def wedderburn_ring(
     ring_cyclic3_gf4: abstract.GroupRing,
     ring_alternating4_gf5: abstract.GroupRing,
 ) -> abstract.GroupRing:
+    """Retrieve a ring for which we have pre-built a Wedderburn-Artin transformer."""
     return ring_cyclic3_gf4 if request.param == "cyclic3_gf4" else ring_alternating4_gf5
