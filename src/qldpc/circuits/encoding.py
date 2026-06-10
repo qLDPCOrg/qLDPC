193f6ef9bc487af2d86ff1a0c57b98504690f3e0
Michael A. Perlin <michael.perlin@jpmchase.com>
2026-06-10 09:21:18 -0700

    fix when to canonicalize

--- src/qldpc/circuits/encoding.py
+++ src/qldpc/circuits/encoding.py
@@ -53,18 +53,18 @@ def get_encoding_tableau(code: codes.QuditCode, *, only_zero: bool = False) -> s
             allow_underconstrained=True,
         )
 
+    # identify logical and gauge operators operators
+    logical_ops = code.get_logical_ops()
+    gauge_ops = code.get_gauge_ops()
+
     # Identify:
     # (1) A minimal generating set of stabilizers.
     # (2) "Candidate" destabilizers that have correct pair-wise (anti-)commutation relations
     #     with the stabilizers, but may contain extra stabilizer, logical, or gauge factors.
     stab_ops = code.get_stabilizer_ops()
-    if len(code.get_stabilizer_ops()) != code.num_checks:
+    if len(stab_ops) != len(code) - code.dimension - code.gauge_dimension:
         stab_ops = code.get_stabilizer_ops(canonicalized=True)
     destab_ops = math.get_dual_basis(math.symplectic_conjugate(stab_ops))
-
-    # identify logical and gauge operators operators
-    logical_ops = code.get_logical_ops()
-    gauge_ops = code.get_gauge_ops()
 
     # remove logical and gauge operator components
     dual_logical_ops = logical_ops.reshape(2, -1)[::-1, :].reshape(logical_ops.shape)
